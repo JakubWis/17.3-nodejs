9b2103ac01c14a08c5605994bf056067d5813579
Jakub Wiśniewski <mrkubasny@gmail.com>
2019-01-29 22:03:55 +0100

    v 0.01

---
+++ program.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    var instruction = input.toString().trim();
+    console.log(instruction);
+    switch(instruction){
+    	case '/exit': 
+	    	process.stdout.write('Quitting app...\n');
+	    	process.exit();
+    	case 'v':
+	    	console.log(process.versions);
+	    	process.exit();
+    	case 'lang': 
+	    	console.log(process.env);
+	    	process.exit();
+    	default: 
+    		process.stderr.write('Wrong instruction. Try again: ');
+    		process.stdin.read();
+    }
+    
+});
